758b6dcd10591ac5a45ee08cdb93f32004f196d0
F. Talha Altinel <talhaaltinel@hotmail.com>
2022-07-16 02:34:00 +0100

    snake is added

--- lib/board.rb
+++ lib/board.rb
@@ -3,11 +3,25 @@ class Board
 
   def initialize(size)
     @size = size
+    @center = @size/2
     @cells = create_board
   end
 
   def create_board
-    Array.new(size) { Array.new(size, ".") }
+    Array.new(@size) { Array.new(@size, ".") }
+  end
+
+  def show_on_center(text)
+    cursor = @center
+    if @center - text.length / 2 >= 0
+      cursor = text.length / 2
+    end
+
+    i = 0
+    text.chars.each do |char|
+      cells[@center][@center - cursor + i] = char
+      i += 1
+    end
   end
 end
 
---
+++ lib/snake.rb
@@ -0,0 +1,71 @@
+class Snake
+  attr_reader :size, :direction, :position, :parts
+
+  def initialize(max_x, max_y, size)
+    @size = size
+    @direction = :left
+    @parts = []
+    set_random_position(max_x, max_y)
+    create_snake
+  end
+
+  def create_snake
+    @size.times do |i|
+      @parts.append([position[0], position[1] + i])
+    end
+  end
+
+  def set_random_position(max_x, max_y)
+    @position = [Random.rand(0..max_x-1), Random.rand(0..max_y-1)]
+  end
+
+  def head
+    parts.first unless parts.nil?
+  end
+
+  def body
+    parts[1..parts.length-1] unless parts.nil?
+  end
+
+  def grow
+    @size += 1
+    @parts.append(parts.last) unless parts.last.nil?
+  end
+
+  def set_head(index, value)
+    head[index] = value
+  end
+
+  def turn(key_code)
+    case key_code.chr.downcase
+    when 'w'
+      @direction = :up
+    when 's'
+      @direction = :down
+    when 'a'
+      @direction = :left
+    when 'd'
+      @direction = :right
+    else
+      @direction = :up
+    end
+  end
+
+  def move
+    new_head = [head.first,head.last]
+    case direction
+    when :left
+      new_head[1] -= 1
+    when :right
+      new_head[1] += 1
+    when :up
+      new_head[0] -= 1
+    when :down
+      new_head[0] += 1
+    else
+      return
+    end
+    parts.unshift(new_head)
+    parts.pop unless parts.nil?
+  end
+end
